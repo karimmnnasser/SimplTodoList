a559aa7a4d0f7a42e7f0d5aed64fe3ad455c8919
karim nasser <55085892+karimmnnasser@users.noreply.github.com>
2020-06-16 14:55:08 +0200

    Updates

---
+++ precache-manifest.b06f1deb1c94cca429b76f73294b87ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8cbfe1b1ed65398ea73a0b75c329d19",
+    "url": "/karimmnnasser/SimplTodoList.git/index.html"
+  },
+  {
+    "revision": "e3c1558f2d15edd37a0d",
+    "url": "/karimmnnasser/SimplTodoList.git/static/css/main.1315ae52.chunk.css"
+  },
+  {
+    "revision": "e15610c1fad1053a8042",
+    "url": "/karimmnnasser/SimplTodoList.git/static/js/2.ec0d45b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/karimmnnasser/SimplTodoList.git/static/js/2.ec0d45b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3c1558f2d15edd37a0d",
+    "url": "/karimmnnasser/SimplTodoList.git/static/js/main.9ae597aa.chunk.js"
+  },
+  {
+    "revision": "42d9c93c1bf984b98c25",
+    "url": "/karimmnnasser/SimplTodoList.git/static/js/runtime-main.a701cd73.js"
+  }
+]);
